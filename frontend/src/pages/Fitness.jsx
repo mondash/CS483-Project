1e5c145c10e64a1677505a9edb6fd08f8bf6ecab
austinsteele1998 <austinsteele1998@gmail.com>
2019-10-23 10:42:17 -0500

    Added CSS styles for Fitness Page

--- frontend/src/pages/Fitness.jsx
+++ frontend/src/pages/Fitness.jsx
@@ -1,19 +1,20 @@
 import React from 'react';
 import Layout from '../components/shared/Layout';
+import '../components/shared/Fitness.css'
 
 const Fitness = () => (
     <Layout>
         <h1>Fitness</h1>
         <br />
         <h2>Hours</h2>
-        <ul>
+        <ul className = "hours">
             <li>Monday - Friday - 7:00 AM to 8:00 PM</li>
             <li>Saturday - 7:00 AM to 5:00 PM</li>
             <li>Sunday - 11:00 AM to 5:00 PM</li>
         </ul>
         <br />
         <h2>Price</h2>
-        <ul>
+        <ul className = "hours">
             <li>$20.00 per month</li>
             <li>$3.00 per day</li>
             <li>Buy 4 months, get the 5th month FREE</li>
